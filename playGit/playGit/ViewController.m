846a7c5b7f4df2867171d4296e51fd1d2b41d1eb
wangchao0802 <wangchao0802@163.com>
2016-03-27 10:49:07 +0800

    oh branch

--- playGit/playGit/ViewController.m
+++ playGit/playGit/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     NSLog(@"hello world");
+    NSLog(@"this is a branch ");
 }
 
 - (void)didReceiveMemoryWarning {
