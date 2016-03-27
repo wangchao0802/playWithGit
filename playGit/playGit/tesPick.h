7245e49672322464a23a3d3aac6919a51ef2da68
wangchao0802 <wangchao0802@163.com>
2016-03-27 11:12:29 +0800

    come from master cherry pick

---
+++ playGit/playGit/tesPick.h
@@ -0,0 +1,13 @@
+//
+//  tesPick.h
+//  playGit
+//
+//  Created by clark on 3/27/16.
+//  Copyright © 2016 clark. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface tesPick : NSObject
+
+@end
---
+++ playGit/playGit/tesPick.m
@@ -0,0 +1,13 @@
+//
+//  tesPick.m
+//  playGit
+//
+//  Created by clark on 3/27/16.
+//  Copyright © 2016 clark. All rights reserved.
+//
+
+#import "tesPick.h"
+
+@implementation tesPick
+
+@end
